061128ca7b70761dfa8089b9da354a45851c7bf7
Alessio Lodato <alss.ldt@gmail.com>
2024-07-24 21:17:03 +0000

    created and styled gamepage and answer containers

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,111 +1,63 @@
-const myQuestions = [
+// Game Questions and Answers
+const questions = [
     {
-      question: "This is a picture of the skyline of the largest city in South America.Which is:",
-      answers: {
-        a: "Buenos Aires",
-        b: "Sao Paulo",
-        c: "Lima",
-        d: "Bogota"
-      },
-      info: "São Paulo is the most populous city in South America and Brazil, and is the capital of the state of São Paulo.Is the largest urban area by population outside Asia and the most populous Portuguese-speaking city in the world.",
-      correctAnswer: "b"
+      question: 'This is a picture of the skyline of the largest city in South America.Which is:',
+      answers: ['Buenos Aires','Sao Paulo','Lima','Bogota'],
+      correctAnswer: 'Sao Paulo',
+      info: 'São Paulo is the most populous city in South America and Brazil, and is the capital of the state of São Paulo.Is the largest urban area by population outside Asia and the most populous Portuguese-speaking city in the world.'
     },
     {
-      question: "This is the picture of a cenote. In which of the following countries it can be found?",
-      answers: {
-        a: "Portugal",
-        b: "New Zealand",
-        c: "Maldives",
-        d: "Mexico"
-      },
-      info: "A cenote is a natural pit, or sinkhole, resulting when a collapse of limestone bedrock exposes groundwater. The term originated on the Yucatán Peninsula of Mexico, where the ancient Maya commonly used cenotes for water supplies, and occasionally for sacrificial offerings.",
-      correctAnswer: "d"
+      question: 'This is the picture of a "Cenote". In which of the following countries it can be found?',
+      answers: ['Portugal','New Zealand','Maldives','Mexico'],
+      correctAnswer: 'Mexico',
+      info: 'A cenote is a natural pit, or sinkhole, resulting when a collapse of limestone bedrock exposes groundwater. The term originated on the Yucatán Peninsula of Mexico, where the ancient Maya commonly used cenotes for water supplies, and occasionally for sacrificial offerings.'
     },
     {
-      question: "In which city is this building located?",
-      answers: {
-        a: "Sydney",
-        b: "Valencia",
-        c: "Genoa",
-        d: "Porto"
-      },
-      info:"L'Oceanogràfic ('The Oceanographic') is an oceanarium situated on the southeast of the city center of Valencia, Spain, where different marine habitats are represented.",
-      correctAnswer: "b"
+      question: 'In which city is this building located?',
+      answers: ['Sydney','Valencia','Genoa','Porto'],
+      correctAnswer: 'Valencia',
+      info:"L'Oceanogràfic ('The Oceanographic') is an oceanarium situated on the southeast of the city center of Valencia, Spain, where different marine habitats are represented."
     },
     {
-        question: "Where was this picture taken?",
-        answers: {
-          a: "United States",
-          b: "Spain",
-          c: "Morocco",
-          d: "Argentina"
-        },
-        info:"Monument Valley is a region of the Colorado Plateau characterized by a cluster of sandstone buttes, with the largest reaching 1,000 ft (300 m) above the valley floor.The most famous butte formations are located in northeastern Arizona along the Utah/Arizona state line. The valley is considered sacred by the Navajo Nation, the Native American people within whose reservation it lies.",
-        correctAnswer: "a"
-      },
-      {
-        question: "Where was this picture taken?",
-        answers: {
-          a: "Netherlands",
-          b: "France",
-          c: "Italy",
-          d: "Spain"
-        },
-        info:"Venice is a city in northeastern Italy and the capital of the Veneto region. It is built on a group of 126 islands that are separated by expanses of open water and by canals; portions of the city are linked by 472 bridges.",
-      },
-      {
-        question: "Where is this place located?",
-        answers: {
-          a: "France",
-          b: "Germany",
-          c: "United Kingdom",
-          d: "Italy"
-        },
-        info:"Mont-Saint-Michel is a tidal island and mainland commune in Normandy, France.The commune's position on an island just a few hundred metres from land made it accessible at low tide to the many pilgrims to its abbey, and defensible as the incoming tide stranded, drove off, or drowned assailants.Mont-Saint-Michel and its surrounding bay were inscribed on the UNESCO list of World Heritage Sites in 1979 for its unique aesthetic and importance as a Catholic site. It is visited by more than three million people each year.",
-        correctAnswer: "a"
-      },
-      {
-        question: "Where is this place located?",
-        answers: {
-          a: "Chile",
-          b: "United States",
-          c: "China",
-          d: "Australia"
-        },
-        info:"Uluru, also known as Ayers Rock,is a large sandstone monolith. It outcrops near the centre of Australia in the southern part of the Northern Territory.The area around the formation is home to an abundance of springs, waterholes, rock caves and ancient paintings and Uluru is listed as a UNESCO World Heritage Site.",
-        correctAnswer: "d"
-      },
-      {
-        question: "Where is this place located?",
-        answers: {
-          a: "Turkey",
-          b: "Saudi Arabia",
-          c: "Greece",
-          d: "Morocco"
-        },
-        info:"Chefchaouen is a city in northwest Morocco. It is the chief town of the province of the same name situated in a montainous region in Nothern Morocco and is noted for its buildings in shades of blue, for which it is nicknamed the 'Blue City'.",
-        correctAnswer: "d"
-      },
-      {
-        question: "This is the skyline of which asian city?",
-        answers: {
-          a: "Seoul",
-          b: "Singapore",
-          c: "Shanghai",
-          d: "Hong Kong"
-        },
-        info:"Shanghai, Lujiazui in particular,has numerous skyscrapers, making it the fifth city in the world with the most skyscrapers.Among the most prominent examples are the 421 m high Jin Mao Tower, the 492 m high Shanghai World Financial Center, and the 632 m high Shanghai Tower, which is the tallest building in China and the third tallest in the world.",
-        correctAnswer: "c"
-      },
-      {
-        question: "Where is thi salty desert located?",
-        answers: {
-          a: "Bolivia",
-          b: "Algeria",
-          c: "Mongolia",
-          d: "Namibia"
-        },
-        info:"Salar de Uyuni is the world's largest salt flat, at over 10,000 square kilometres in area.It is located in southwest Bolivia in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 m above sea level.The Salar was formed as a result of transformations between several prehistoric lakes that existed around forty thousand years ago but had all evaporated over time. It is now covered by a few meters of salt crust, which has an extraordinary flatness with the average elevation variations within one meter over the entire area of the Salar.",
-        correctAnswer: "a"
-      }
+      question: 'Where was this picture taken?',
+      answers: ['United States','Spain','Morocco','Argentina' ],
+      correctAnswer: 'United States',
+      info:'Monument Valley is a region of the Colorado Plateau characterized by a cluster of sandstone buttes, with the largest reaching 1,000 ft (300 m) above the valley floor.The most famous butte formations are located in northeastern Arizona along the Utah/Arizona state line. The valley is considered sacred by the Navajo Nation, the Native American people within whose reservation it lies.'
+    },
+    {
+      question: 'Where was this picture taken?',
+      answers: ['Netherlands','France','Italy','Spain'],
+      correctAnswer: 'Italy',
+      info:'Venice is a city in northeastern Italy and the capital of the Veneto region. It is built on a group of 126 islands that are separated by expanses of open water and by canals; portions of the city are linked by 472 bridges.'
+    },
+    {
+      question: 'Where is this place located?',
+      answers: ['France','Germany','United Kingdom','Italy'],
+      correctAnswer: 'France',
+      info:"Mont-Saint-Michel is a tidal island and mainland commune in Normandy, France.The commune's position on an island just a few hundred metres from land made it accessible at low tide to the many pilgrims to its abbey, and defensible as the incoming tide stranded, drove off, or drowned assailants.Mont-Saint-Michel and its surrounding bay were inscribed on the UNESCO list of World Heritage Sites in 1979 for its unique aesthetic and importance as a Catholic site. It is visited by more than three million people each year."
+    },
+    {
+      question: 'Where is this place located?',
+      answers: ['Chile','United States','China','Australia'],
+      correctAnswer: 'Australia',
+      info:'Uluru, also known as Ayers Rock,is a large sandstone monolith. It outcrops near the centre of Australia in the southern part of the Northern Territory.The area around the formation is home to an abundance of springs, waterholes, rock caves and ancient paintings and Uluru is listed as a UNESCO World Heritage Site.'
+    },
+    {
+      question: 'Where is this place located?',
+      answers: ['Turkey','Saudi Arabia','Greece','Morocco'],
+      correctAnswer: 'Morocco',
+      info:'Chefchaouen is a city in northwest Morocco. It is the chief town of the province of the same name situated in a montainous region in Nothern Morocco and is noted for its buildings in shades of blue, for which it is nicknamed the "Blue City".'
+    },
+    {
+      question: 'This is the skyline of which asian city?',
+      answers: ['Seoul','Singapore','Shanghai','Hong Kong'],
+      correctAnswer: 'Shanghai',
+      info:'Shanghai, Lujiazui in particular,has numerous skyscrapers, making it the fifth city in the world with the most skyscrapers.Among the most prominent examples are the 421 m high Jin Mao Tower, the 492 m high Shanghai World Financial Center, and the 632 m high Shanghai Tower, which is the tallest building in China and the third tallest in the world.'
+    },
+    {
+      question: 'Where is this salty desert located?',
+      answers: ['Bolivia','Algeria','Mongolia','Namibia'],
+      correctAnswer: 'Bolivia',
+      info:"Salar de Uyuni is the world's largest salt flat, at over 10,000 square kilometres in area.It is located in southwest Bolivia in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 m above sea level.The Salar was formed as a result of transformations between several prehistoric lakes that existed around forty thousand years ago but had all evaporated over time. It is now covered by a few meters of salt crust, which has an extraordinary flatness with the average elevation variations within one meter over the entire area of the Salar."       
+    }
   ];
--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-let questionAnswers = document.getElementById("question-answers");
-let questionImage = document.getElementById("question-img");
-});
-
