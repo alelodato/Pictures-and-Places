5ce5ea5c4aba964dc947ebf002c3fb28783808cc
alelodato <alss.ldt@gmail.com>
2025-06-30 10:15:40 +0200

    moved player name form to the end of the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,79 +2,73 @@
  * Game Variables
  */
 // Game title,intro message and start button variables
-const title = document.getElementById('title');
-const intro = document.getElementById('intro');
-const playerName = document.getElementById('player-name');
-const newGame = document.getElementById('new-game-btn');
-const startButton = document.getElementById("start-button");
-const start = document.querySelector('.title-intro-start');
-const playerNameForm = document.getElementById('player-name-form');
+const title = document.getElementById("title");
+const intro = document.getElementById("intro");
+const playerName = document.getElementById("player-name");
+const newGame = document.getElementById("new-game-btn");
+const nameSubmit = document.getElementById("player-name-form");
+const start = document.querySelector(".title-intro-start");
+const playerNameForm = document.getElementById("player-name-form");
 // Questions, answers buttons, quiz images, next button and score tracker variables
-const questionContainer = document.getElementById('question-container');
-const quizImage = document.getElementById('quiz-image');
-const answerButtons = document.getElementById('answer-buttons');
-const nextButton = document.getElementById('next');
-const scoreTracker = document.getElementById('score');
+const questionContainer = document.getElementById("question-container");
+const quizImage = document.getElementById("quiz-image");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next");
+const scoreTracker = document.getElementById("score");
 //Variable to get the finish button
-const getResults = document.getElementById('results');
+const getResults = document.getElementById("results");
 // End game and restart button variable
-const endMessage = document.getElementById('quiz-end');
-const restartButton = document.getElementById('restart');
-const restartGameBtn = document.getElementById('start-again');
-const leaderBtn = document.getElementById('leader-btn');
-const leaderBoard = document.getElementById('leaderboard');
-const gameBackground = document.getElementById('homepage');
+const endMessage = document.getElementById("quiz-end");
+const restartButton = document.getElementById("restart");
+const restartGameBtn = document.getElementById("start-again");
+const leaderBtn = document.getElementById("leader-btn");
+const leaderBoard = document.getElementById("leaderboard");
+const gameBackground = document.getElementById("homepage");
 // Variables for shuffled questions and the current question index
 let shuffledQuestions, currentQuestionIndex;
 // Variable to get the questions info text content
 let questionsInfo;
-let leaderList = document.getElementById('board');
+let leaderList = document.getElementById("board");
 // Empty array to store scores to render into the leaderboard
 let scores = [];
 
 /**
  * Event Listeners
  */
-newGame.addEventListener('click', insertName);
-startButton.addEventListener('submit', startGame);
-nextButton.addEventListener('click', nextQuestion);
-restartGameBtn.addEventListener('click', startGame);
-getResults.addEventListener('click', endQuiz);
-restartButton.addEventListener('click', startGame);
-leaderBtn.addEventListener('click', showLeaderboard);
 
-function insertName() {
-  // Hide title,intro message and start button, to make space for the quiz game content
-  title.classList.add('hide');
-  intro.classList.add('hide');
-  newGame.classList.add('hide');
-  playerNameForm.classList.remove('hide');
-}
+document.addEventListener("DOMContentLoaded", function () {
+  newGame.addEventListener("click", startGame);
+  nextButton.addEventListener("click", nextQuestion);
+  restartGameBtn.addEventListener("click", startGame);
+  getResults.addEventListener("click", insertName);
+  nameSubmit.addEventListener("submit", endQuiz);
+  restartButton.addEventListener("click", startGame);
+  leaderBtn.addEventListener("click", showLeaderboard);
+});
 
 /**
  * Function that starts the game when start button clicked
  */
 function startGame() {
-  scores.push(`name:${playerName}`)
-  gameBackground.style.backgroundColor = 'rgb(0 0 0 / 66%)';
-  start.classList.add('hide');
-  playerNameForm.classList.remove('hide');
+  scores.push(`name:${playerName}`);
+  gameBackground.style.backgroundColor = "rgb(0 0 0 / 66%)";
+  start.classList.add("hide");
   // Remove 'hide' class from quiz content to show when game starts
-  questionContainer.classList.remove('hide');
-  answerButtons.classList.remove('hide');
-  nextButton.classList.remove('hide');
-  scoreTracker.classList.remove('hide');
-  quizImage.classList.remove('hide');
-  restartGameBtn.classList.remove('hide');
+  questionContainer.classList.remove("hide");
+  answerButtons.classList.remove("hide");
+  nextButton.classList.remove("hide");
+  scoreTracker.classList.remove("hide");
+  quizImage.classList.remove("hide");
+  restartGameBtn.classList.remove("hide");
   // Hide end message to show only when finish button is pressed
-  endMessage.classList.add('hide');
+  endMessage.classList.add("hide");
   // Hide restart button to show only when end message is shown
-  restartButton.classList.add('hide');
-  leaderBtn.classList.add('hide');
-  leaderBoard.classList.add('hide');
+  restartButton.classList.add("hide");
+  leaderBtn.classList.add("hide");
+  leaderBoard.classList.add("hide");
   // Set score tracker to 0 at the beginning of the game
-  document.getElementById('right').textContent = 0;
-  document.getElementById('wrong').textContent = 0;
+  document.getElementById("right").textContent = 0;
+  document.getElementById("wrong").textContent = 0;
   // Set questions to appear in random order
   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
@@ -107,22 +101,21 @@ function showQuestion(questions) {
   quizImage.innerHTML = "";
   // Define questionsInfo variable to show infos related to the question, as this function has 'questions' as attribute
   questionsInfo = questions.info;
-  // Create img element and assign correct imageUrl to the current question, so the related image is going to show 
-  const image = document.createElement('img');
+  // Create img element and assign correct imageUrl to the current question, so the related image is going to show
+  const image = document.createElement("img");
   image.src = questions.imageUrl;
   quizImage.appendChild(image);
 
-  
   // Create answers buttons, and fills them with text related to the current question
-  questions.answers.forEach(answers => {
-    const button = document.createElement('button');
+  questions.answers.forEach((answers) => {
+    const button = document.createElement("button");
     button.textContent = answers.text;
     answerButtons.appendChild(button);
     // Event listener to call the checkAnswer function when answer button is clicked
-    button.addEventListener('click', checkAnswer);
+    button.addEventListener("click", checkAnswer);
     if (answers.correct) {
       button.dataset.correct = answers.correct;
-  }
+    }
   });
 }
 
@@ -135,93 +128,102 @@ function checkAnswer(event) {
   const selectedAnswer = event.target;
   const correct = selectedAnswer.dataset.correct === "true";
   // Check if the selected answer is correct, assigning styles to the buttons and showing an alert message
-  if (correct){
+  if (correct) {
     selectedAnswer.style.backgroundColor = "lightgreen";
     // Increments score tracker for correct answers
     incrementCorrect();
     Swal.fire({
-      title: 'Correct!',
-      text: (` ${questionsInfo}`),
-      icon: 'success',
-      confirmButtonText: 'OK'
-  });
-  } else {selectedAnswer.style.backgroundColor = "red";
+      title: "Correct!",
+      text: ` ${questionsInfo}`,
+      icon: "success",
+      confirmButtonText: "OK",
+    });
+  } else {
+    selectedAnswer.style.backgroundColor = "red";
     Swal.fire({
       title: "Sorry! That's not the correct answer...",
-      text: (`${questionsInfo}`),
-      icon: 'error',
-      confirmButtonText: 'OK'
-  });
+      text: `${questionsInfo}`,
+      icon: "error",
+      confirmButtonText: "OK",
+    });
     let correctAnswer = document.querySelector("[data-correct='true']");
     correctAnswer.style.backgroundColor = "lightgreen";
-    // Increments score tracker for incorrect answers 
+    // Increments score tracker for incorrect answers
     incrementIncorrect();
   }
   // Disallows answer buttons to be clicked after answer is submitted
   answerButtons.style.pointerEvents = "none";
   // Checks if the user reached the last question, and in that case hides the next button and shows the finish button to allow to finish the game and show the results
   if (currentQuestionIndex === questions.length) {
-    nextButton.classList.add('hide');
-    getResults.classList.remove('hide');
-  } else {nextButton.classList.remove('hide');
-  scoreTracker.classList.remove('hide');
+    nextButton.classList.add("hide");
+    getResults.classList.remove("hide");
+  } else {
+    nextButton.classList.remove("hide");
+    scoreTracker.classList.remove("hide");
   }
 }
 
 /**
- * Function to reset answer buttons for current question, and to hide next button and score tracker 
+ * Function to reset answer buttons for current question, and to hide next button and score tracker
  */
 function resetAnswers() {
   answerButtons.innerHTML = "";
-  nextButton.classList.add('hide');
-  scoreTracker.classList.add('hide');
+  nextButton.classList.add("hide");
+  scoreTracker.classList.add("hide");
 }
 
 /**
  * Function to increment incorrect answer score when incorrect answer is selected by the user
  */
-function incrementCorrect () {
-  let previousCorrect = parseInt(document.getElementById('right').textContent);
-    document.getElementById('right').textContent = previousCorrect + 1;
-
+function incrementCorrect() {
+  let previousCorrect = parseInt(document.getElementById("right").textContent);
+  document.getElementById("right").textContent = previousCorrect + 1;
 }
 
 /**
  * Function to increment correct answer score when correct answer is selected by the user
  */
-function incrementIncorrect () {
-  let previousIncorrect = parseInt(document.getElementById('wrong').textContent);
-    document.getElementById('wrong').textContent = previousIncorrect + 1;
+function incrementIncorrect() {
+  let previousIncorrect = parseInt(
+    document.getElementById("wrong").textContent
+  );
+  document.getElementById("wrong").textContent = previousIncorrect + 1;
+}
 
+function insertName() {
+  // Hide all the quiz content to make space for results and end game message
+  questionContainer.classList.add("hide");
+  answerButtons.classList.add("hide");
+  quizImage.classList.add("hide");
+  getResults.classList.add("hide");
+  restartGameBtn.classList.add("hide");
+  playerNameForm.classList.remove("hide");
 }
 
 /**
  * Function to show quiz game end message and results, when finish button is pressed
  */
-function endQuiz(){
-  // Hide all the quiz content to make space for results and end game message
-  questionContainer.classList.add('hide');
-  answerButtons.classList.add('hide');
-  quizImage.classList.add('hide');
-  getResults.classList.add('hide');
-  restartGameBtn.classList.add('hide');
+function endQuiz(event) {
+  console.log(endQuiz);
+  event.preventDefault();
+  playerNameForm.classList.add("hide");
   // Shows end game results, message and restart game button
-  endMessage.classList.remove('hide');
-  restartButton.classList.remove('hide');
-  scoreTracker.classList.remove('hide');
-  leaderBtn.classList.remove('hide');
-  let finalScore = parseInt(document.getElementById('right').textContent);
+  endMessage.classList.remove("hide");
+  restartButton.classList.remove("hide");
+  scoreTracker.classList.remove("hide");
+  leaderBtn.classList.remove("hide");
+  let finalScore = parseInt(document.getElementById("right").textContent);
   scores.push(`score:${finalScore}`);
 }
 
 function showLeaderboard() {
   // Shows the game leaderboard
-  scoreTracker.classList.add('hide');
-  leaderBtn.classList.add('hide');
-  endMessage.classList.add('hide');
-  leaderBoard.classList.remove('hide');
+  scoreTracker.classList.add("hide");
+  leaderBtn.classList.add("hide");
+  endMessage.classList.add("hide");
+  leaderBoard.classList.remove("hide");
   // Rendering the leaderboard
-  scores.sort((a, b) => b.score- a.score);
+  scores.sort((a, b) => b.score - a.score);
   leaderList.innerHTML = "";
   scores.forEach((player, index) => {
     const listItem = document.createElement("li");
