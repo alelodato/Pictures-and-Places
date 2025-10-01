d6bf7f6cb157e2554daf3d78ad67d6fce3a878bf
alelodato <alss.ldt@gmail.com>
2025-10-01 19:11:13 +0700

    adjusted quiz images format to improve performances

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -10,7 +10,7 @@ const questions = [
       {text:'Lima', correct:false},
       {text:'Bogota', correct:false}
   ],
-    imageUrl: "assets/images/quiz-img/sao-paulo.jpeg",
+    imageUrl: "assets/images/quiz-img/sao-paulo.webp",
     info: 'São Paulo is the most populous city in South America and Brazil, and is the capital of the state of São Paulo.Is the largest urban area by population outside Asia and the most populous Portuguese-speaking city in the world.'
   },
   {
@@ -21,7 +21,7 @@ const questions = [
       {text:'Maldives', correct:false},
       {text:'Mexico', correct:true}
   ],
-    imageUrl: "assets/images/quiz-img/cenote.jpeg",
+    imageUrl: "assets/images/quiz-img/cenote.webp",
     info: 'A cenote is a natural pit, or sinkhole, resulting when a collapse of limestone bedrock exposes groundwater. The term originated on the Yucatán Peninsula of Mexico, where the ancient Maya commonly used cenotes for water supplies, and occasionally for sacrificial offerings.'
   },
   {
@@ -32,7 +32,7 @@ const questions = [
       {text:'Genoa', correct:false},
       {text:'Porto',correct:false}
   ],
-    imageUrl: "assets/images/quiz-img/oceanographic.jpeg",
+    imageUrl: "assets/images/quiz-img/oceanographic.webp",
     info:"L'Oceanogràfic ('The Oceanographic') is an oceanarium situated on the southeast of the city center of Valencia, Spain, where different marine habitats are represented."
   },
   {
@@ -43,7 +43,7 @@ const questions = [
       {text:'Morocco', correct:false},
       {text:'Argentina', correct:false}
   ],
-    imageUrl: "assets/images/quiz-img/monument-valley.jpeg",
+    imageUrl: "assets/images/quiz-img/monument-valley.webp",
     info:'Monument Valley is a region of the Colorado Plateau characterized by a cluster of sandstone buttes, with the largest reaching 1,000 ft (300 m) above the valley floor. The most famous butte formations are located in northeastern Arizona along the Utah/Arizona state line. The valley is considered sacred by the Navajo Nation, the Native American people within whose reservation it lies.'
   },
   {
@@ -54,7 +54,7 @@ const questions = [
       {text:'Italy', correct:true},
       {text:'Spain', correct:false}
   ],
-  imageUrl: "assets/images/quiz-img/venice.jpeg",
+  imageUrl: "assets/images/quiz-img/venice.webp",
     info:'Venice is a city in northeastern Italy and the capital of the Veneto region. It is built on a group of 126 islands that are separated by expanses of open water and by canals; portions of the city are linked by 472 bridges.'
   },
   {
@@ -65,7 +65,7 @@ const questions = [
       {text:'United Kingdom', correct:false},
       {text:'Italy', correct:false}
   ],
-    imageUrl: "assets/images/quiz-img/mont-st-michel.jpeg",
+    imageUrl: "assets/images/quiz-img/mont-st-michel.webp",
     info:"Mont-Saint-Michel is a tidal island and mainland commune in Normandy, France. The commune's position on an island just a few hundred metres from land made it accessible at low tide to the many pilgrims to its abbey, and defensible as the incoming tide stranded, drove off, or drowned assailants.Mont-Saint-Michel and its surrounding bay were inscribed on the UNESCO list of World Heritage Sites in 1979 for its unique aesthetic and importance as a Catholic site. It is visited by more than three million people each year."
   },
   {
@@ -76,7 +76,7 @@ const questions = [
       {text:'China', correct:false},
       {text:'Australia', correct:true}
   ],
-    imageUrl: "assets/images/quiz-img/uluru.jpeg",
+    imageUrl: "assets/images/quiz-img/uluru.webp",
     info:'Uluru, also known as Ayers Rock,is a large sandstone monolith. It outcrops near the centre of Australia in the southern part of the Northern Territory. The area around the formation is home to an abundance of springs, waterholes, rock caves and ancient paintings and Uluru is listed as a UNESCO World Heritage Site.'
   },
   {
@@ -87,7 +87,7 @@ const questions = [
       {text:'Greece', correct:false},
       {text:'Morocco', correct:true}
   ],
-    imageUrl: "assets/images/quiz-img/chefchaouen.jpg",
+    imageUrl: "assets/images/quiz-img/chefchaouen.webp",
     info:'Chefchaouen is a city in northwest Morocco. It is the chief town of the province of the same name situated in a montainous region in Nothern Morocco and is noted for its buildings in shades of blue, for which it is nicknamed the "Blue City".'
   },
   {
@@ -98,7 +98,7 @@ const questions = [
       {text:'Shanghai', correct:true},
       {text:'Hong Kong', correct:false}
   ],
-    imageUrl: "assets/images/quiz-img/shanghai.jpg",
+    imageUrl: "assets/images/quiz-img/shanghai.webp",
     info:'Shanghai, Lujiazui in particular, has numerous skyscrapers, making it the fifth city in the world with the most skyscrapers. Among the most prominent examples are the 421 m high Jin Mao Tower, the 492 m high Shanghai World Financial Center, and the 632 m high Shanghai Tower, which is the tallest building in China and the third tallest in the world.'
   },
   {
@@ -110,7 +110,7 @@ const questions = [
       {text:'Namibia', correct:false}
   ],
     correctAnswer: 'Bolivia',
-    imageUrl: "assets/images/quiz-img/salar.jpeg",
+    imageUrl: "assets/images/quiz-img/salar.webp",
     info:"Salar de Uyuni is the world's largest salt flat, at over 10,000 square kilometres in area. It is located in southwest Bolivia, near the crest of the Andes at an elevation of 3,656 m above sea level. The Salar was formed as a result of transformations between several prehistoric lakes that existed around forty thousand years ago but had all evaporated over time. It is now covered by a few meters of salt crust, which has an extraordinary flatness with the average elevation variations within one meter over the entire area of the Salar."       
   }
 ];
